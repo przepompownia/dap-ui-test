08a259d3d3ee0ffa47e28e5b84a6b97277f8f08f
przepompownia <przepompownia@users.noreply.github.com>
2022-02-16 11:22:23 +0100

    Fix mapping

--- plugin/dapui.lua
+++ plugin/dapui.lua
@@ -2,5 +2,5 @@ local dapui = require('dapui')
 local keymap = require 'vim.keymap'
 local opts = {silent = true, noremap = true}
 keymap.set({'n'}, ',dt', dapui.toggle, opts)
-keymap.set({'x'}, ',de', function() dapui.eval(nil, {enter = true, context = repl}) end, opts)
+keymap.set({'x'}, ',de', function() dapui.eval(nil, {enter = true, context = 'repl'}) end, opts)
 dapui.setup({})
