bff78146deebc1097c99dc1a99c46fd2a56cbbda
przepompownia <przepompownia@users.noreply.github.com>
2022-04-08 23:14:44 +0200

    Update dap-tab

--- plugin/dap.lua
+++ plugin/dap.lua
@@ -3,17 +3,6 @@ local keymap = require 'vim.keymap'
 local widgets = require('dap.ui.widgets')
 local utils = require('dap.utils')
 
-local opts = {silent = true, noremap = true}
-keymap.set({'x'}, ',dh', function() widgets.hover(utils.get_visual_selection_text) end, opts)
-keymap.set({'n'}, ',dv', dap.step_over, opts)
-keymap.set({'n'}, ',di', dap.step_into, opts)
-keymap.set({'n'}, ',do', dap.step_out, opts)
-keymap.set({'n'}, ',db', dap.toggle_breakpoint, opts)
-keymap.set({'n'}, ',dc', dap.continue, opts)
-keymap.set({'n'}, ',du', dap.up, opts)
-keymap.set({'n'}, ',dd', dap.down, opts)
-keymap.set({'n'}, ',dx', dap.close, opts)
-
 dap.set_log_level('TRACE')
 
 dap.adapters.php = {
@@ -34,7 +23,6 @@ dap.configurations.php = {
       max_children = 512,
       max_data = 1024,
       max_depth = 4,
-      extended_properties = 1,
     },
     breakpoints = {
       exception = {
@@ -48,31 +36,59 @@ dap.configurations.php = {
   }
 }
 
-local debugWinId = nil
 local api = vim.api
-local function openTabForThread(threadId)
+local debugWinId = nil
+
+local function goToDebugWin()
   if nil ~= debugWinId and api.nvim_win_is_valid(debugWinId) then
     api.nvim_set_current_win(debugWinId)
+
+    return true
+  end
+end
+
+local function verboseGoToDebugWin()
+  if goToDebugWin() then
     return
   end
 
-  vim.cmd('tabedit %')
+  vim.notify('Debug window is not defined yet.')
+end
+
+local function closeDebugWin()
+  if nil == debugWinId then
+    return
+  end
+  local tabNr = api.nvim_tabpage_get_number(api.nvim_win_get_tabpage(debugWinId))
+  vim.cmd('tabclose ' .. tabNr)
+end
+
+local function openTabForThread()
+  if goToDebugWin() then
+    return
+  end
+
+  vim.cmd([[
+    tabedit %
+    setlocal scrolloff=10
+  ]])
   debugWinId = vim.fn.win_getid()
-  vim.cmd('autocmd WinClosed ' .. debugWinId .. ' ++once lua require"dapui".close()')
   require('dapui').open()
 end
 
-dap.listeners.before['event_stopped']['arctgx-dap-tab'] = function(session, body)
-  openTabForThread(nil)
+dap.listeners.before['event_stopped']['arctgx-dap-tab'] = function()
+  openTabForThread()
 end
 
-dap.listeners.after['event_thread']['arctgx-dap-tab'] = function(session, body)
-  if body.reason == 'started' then
-    openTabForThread(body.threadId)
-    return
-  end
-  if body.reason == '' then
-  else
-    vim.notify('Reason ' .. body.reason)
-  end
-end
+local opts = {silent = true, noremap = true}
+keymap.set({'n'}, ',dw', verboseGoToDebugWin, opts)
+keymap.set({'n'}, ',dc', closeDebugWin, opts)
+keymap.set({'x'}, ',dh', function() widgets.hover(utils.get_visual_selection_text) end, opts)
+keymap.set({'n'}, ',dv', dap.step_over, opts)
+keymap.set({'n'}, ',di', dap.step_into, opts)
+keymap.set({'n'}, ',do', dap.step_out, opts)
+keymap.set({'n'}, ',db', dap.toggle_breakpoint, opts)
+keymap.set({'n'}, ',dr', dap.continue, opts)
+keymap.set({'n'}, ',du', dap.up, opts)
+keymap.set({'n'}, ',dd', dap.down, opts)
+keymap.set({'n'}, ',dx', dap.close, opts)
