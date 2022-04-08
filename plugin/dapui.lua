c548b33ee0869f45b108dd43ee1bd1916928c484
przepompownia <przepompownia@users.noreply.github.com>
2022-04-09 01:00:45 +0200

    check if win id is valid

--- plugin/dapui.lua
+++ plugin/dapui.lua
@@ -2,7 +2,11 @@ local dapui = require('dapui')
 local keymap = require 'vim.keymap'
 local opts = {silent = true, noremap = true}
 local function jumpToScopes()
-  vim.api.nvim_set_current_win(vim.fn.win_findbuf(vim.fn.bufnr('DAP Scopes'))[1])
+  local winId = vim.fn.win_findbuf(vim.fn.bufnr('DAP Scopes'))[1]
+  if nil == winId or not vim.api.nvim_win_is_valid(winId) then
+    return
+  end
+  vim.api.nvim_set_current_win(winId)
 end
 keymap.set({'n'}, ',dt', dapui.toggle, opts)
 keymap.set({'n'}, ',ds', jumpToScopes, opts)
