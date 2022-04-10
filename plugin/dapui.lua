f37a81d5ac3f97140bbcf8e537c6fb37bf356f8a
przepompownia <przepompownia@users.noreply.github.com>
2022-04-10 17:30:54 +0200

    Update dap-ui with configuration

--- plugin/dapui.lua
+++ plugin/dapui.lua
@@ -1,4 +1,5 @@
 local dapui = require('dapui')
+local dapuiInit = require('dapui.config.init')
 local keymap = require 'vim.keymap'
 local opts = {silent = true, noremap = true}
 local function jumpToScopes()
@@ -11,4 +12,21 @@ end
 keymap.set({'n'}, ',dt', dapui.toggle, opts)
 keymap.set({'n'}, ',ds', jumpToScopes, opts)
 keymap.set({'x'}, ',de', function() dapui.eval(nil, {enter = true, context = 'repl'}) end, opts)
-dapui.setup({})
+dapui.setup({
+  sidebar = {
+    -- You can change the order of elements in the sidebar
+    elements = {
+      -- Provide IDs as strings or tables with "id" and "size" keys
+      {
+        id = dapuiInit.elements.SCOPES,
+        size = 0.25, -- Can be float or integer > 1
+        expand_long_lines = true,
+      },
+      { id = dapuiInit.elements.BREAKPOINTS, size = 0.25, expand_long_lines = false },
+      { id = dapuiInit.elements.STACKS, size = 0.25, expand_long_lines = true },
+      { id = dapuiInit.elements.WATCHES, size = 0.25, expand_long_lines = true },
+    },
+    size = 40,
+    position = "left", -- Can be "left" or "right"
+  },
+})
